334e68ff5878f9496b0f713c997cf391990c7cc3
Obi Martins <obichukwumartins@gmail.com>
2022-06-01 16:35:31 +0100

    c

---
+++ JS/getsolution.js
@@ -0,0 +1,10 @@
+//document.getElementById("demo").innerHTML="You are welcome";
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
